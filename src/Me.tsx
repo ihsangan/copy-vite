8e508c1636118a2064a6a2bd7217b958047ee0d2
ihsangan <me@isan.eu.org>
2024-10-18 18:11:32 +0800

    .

--- src/Me.tsx
+++ src/Me.tsx
@@ -2,26 +2,26 @@ const Home = () => {
   return (
     <div className="min-h-screen">
       <header className="flex justify-center">
-        <h1 className="text-4xl font-bold tracking-wider uppercase mt-12">
+        <h1 className="text-4xl text-center font-bold tracking-wider uppercase mt-12">
           Muhammad Ihsan
         </h1>
       </header>
 
-      <main className="text-center mt-6">
+      <main className="text-center mt-5">
         <p className="text-xl font-medium">
           Someone who wants everything.
         </p>
         <p className="mt-4">
           Technophile, IT-interested, ordinary guy. Nothing special about me. Nothing, until you know me.
         </p>
-        <div className="mt-6 space-x-4">
-          <a href="https://blog.isan.eu.org/" className="underline hover:text-blue-500">blog</a>
-          <a href="links" className="underline hover:text-blue-500">connect</a>
+        <div className="mt-4 space-x-1">
+          <a href="https://blog.isan.eu.org/" className="underline hover:text-blue-500">blog</a> |
+          <a href="links" className="underline hover:text-blue-500">connect</a> | 
           <a href="mailto:me@isan.eu.org" className="underline hover:text-blue-500">me@isan.eu.org</a>
         </div>
       </main>
 
-      <footer className="text-center mt-12">
+      <footer className="text-center mt-6">
         <p>©219-2024</p>
       </footer>
     </div>
